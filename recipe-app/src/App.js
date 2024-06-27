609349b2af42e4f3768445cff8dae8d7b3bab0c6
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-06-27 16:54:47 +0530

    update

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,26 +1,13 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AddRecipePage from './components/AddRecipePage';
-import RecipeDetailPage from './components/RecipeDetailPage';
-import RecipeListPage from './components/RecipeListPage.';
+
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
-  const [recipes, setRecipes] = useState([]);
-
-  const addRecipe = (recipe) => {
-    setRecipes([...recipes, recipe]);
-  };
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<RecipeListPage recipes={recipes} />} />
-        <Route path="/add" element={<AddRecipePage addRecipe={addRecipe} />} />
-        <Route path="/recipe/:id" element={<RecipeDetailPage recipes={recipes} />} />
-      </Routes>
-    </Router>
-  );
+  return(
+    <div className='app'>
+    <TodoList/>
+    </div>
+  )
 }
 
 export default App;
