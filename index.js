51891cf0b323e2b3d8242e08cf20e9f77c7bbff4
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-07-07 20:02:41 +0530

    update

---
+++ index.js
@@ -0,0 +1,44 @@
+//  let rows = 5;
+//  let char = '';
+
+//  for (let i=1;i<=rows;i++) {
+//     for(let j = 1; j<=i;j++){
+//         char += j;
+//         // console.log(j ,'j');
+//     }
+//     char += "\n"
+//     // console.log(i,"i");
+//  }
+//  console.log(char);
+
+
+// let rows = 5;
+// let char = '';
+
+// for (let i=rows;i>=1;i--) {
+//    for(let j = 5; j>=i;j--){
+//        char += j;
+//        // console.log(j ,'j');
+
+//    }
+//    char += "\n"
+//    // console.log(i,"i");
+// }
+// console.log(char);
+
+
+// let num = 5;
+// for(let i = 1; i<=num; i++){
+//     let str = "*";
+//     console.log(str.repeat(i));
+// }
+
+// check whether numbers from 1 to 10 are even or odd and print the result to the console.
+
+// for(let i = 1 ; i<=10;i++){
+//    if(i%2===0){
+//       console.log( i + " is even");
+//    }else{
+//     console.log( i + " is odd");
+//    };
+// };
